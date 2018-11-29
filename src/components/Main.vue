c8838855b3b955252c77fa0f410b71c0f3344f98
ericvanwagoner <44907975+ericvanwagoner@users.noreply.github.com>
2018-11-29 12:54:52 -0700

    Edits11/28

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12" style="">
-            <h3 class="display-3 text-secondary text-uppercase" style="">
+            <h3 class="display-4 text-secondary text-uppercase">
               Marketing Movement Money
             </h3>
           </div>
@@ -34,10 +34,11 @@
         <div class="row">
           <div class="col-12 py-3 pb-5">
             <h3 class="display-1 text-uppercase" style="">Rise</h3>
-            <h1 class="text-secondary pb-1">
-              March {{ date }} at {{ location }}
-            </h1>
-            <h3 class="display-5 text-uppercase" style="">
+            <h1 class="text-secondary pb-1">March 2nd 9AM-6PM</h1>
+            <h3
+              class="display-5 text-uppercase"
+              style="color:var(--red);font-weight:bold"
+            >
               Only 50 Spots Available
             </h3>
             <a class="btn btn-outline-secondary btn-lg py-3 px-5" href="#"
@@ -50,38 +51,17 @@
     <div class="bg-light py-4">
       <div class="container">
         <div class="row">
-          <div class="col-md-12 d-flex justify-content-center">
-            <h3 class="text-primary display-4 text-secondary p-3 bg-light mb-2">
-              WHAT IS THE RISE EVENT?
-            </h3>
-          </div>
+          <div class="col-md-12 d-flex justify-content-center"></div>
         </div>
         <div class="row">
           <div
             class="px-md-5 p-3 col-md-6 d-flex flex-column align-items-start justify-content-center"
           >
-            <h1 class="text-dark">A wonderful serenity</h1>
+            <h1 class="text-dark display-3">Marketing</h1>
             <p class="mb-3 lead text-dark">
               Has taken possession of my entire soul, like these sweet mornings
               of spring which I enjoy with my whole heart.
             </p>
-            <p class="mb-2">Choose an option</p>
-            <div class="row">
-              <div class="col-md-12">
-                <i class="fa fa-stop-circle fa-3x mr-3 text-muted d-inline"></i>
-                <i class="fa fa-circle-o fa-3x mx-3 text-muted d-inline"></i>
-                <i
-                  class="fa fa-stop-circle-o fa-3x mx-3 text-muted d-inline"
-                ></i>
-                <i class="fa fa-circle fa-3x ml-3 text-muted d-inline"></i>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <img
-              class="img-fluid d-block"
-              src="https://static.pingendo.com/img-placeholder-tablet.svg"
-            />
           </div>
         </div>
       </div>
@@ -89,14 +69,8 @@
     <div class="align-items-center d-flex bg-light" style="">
       <div class="container py-2">
         <div class="row d-flex justify-content-end">
-          <div class="col-md-6 px-md-5">
-            <img
-              class="d-block img-fluid"
-              src="https://static.pingendo.com/img-placeholder-tablet.svg"
-            />
-          </div>
           <div class="col-md-6 px-md-5 text-dark">
-            <h1 class="display-3 text-dark mb-0">O my friend</h1>
+            <h1 class="text-dark display-3">Movement</h1>
             <p class="lead mb-4">
               Heaven and earth seem to dwell in my soul and absorb its power,
               like the form of a beloved mistress, then I often think with
@@ -111,45 +85,28 @@
       <div class="container">
         <div class="row">
           <div
-            class="px-md-5 p-3 col-md-6 d-flex flex-column align-items-start justify-content-center"
+            class="px-md-5 p-3 col-md-6 d-flex flex-column align-items-start justify-content-center text-align center"
           >
-            <h1 class="text-dark">A wonderful serenity</h1>
+            <h1 class="text-dark display-3">Money</h1>
             <p class="mb-3 lead text-dark">
               Has taken possession of my entire soul, like these sweet mornings
               of spring which I enjoy with my whole heart.
             </p>
-            <p class="mb-2">Choose an option</p>
-            <div class="row">
-              <div class="col-md-12">
-                <i class="fa fa-stop-circle fa-3x mr-3 text-muted d-inline"></i>
-                <i class="fa fa-circle-o fa-3x mx-3 text-muted d-inline"></i>
-                <i
-                  class="fa fa-stop-circle-o fa-3x mx-3 text-muted d-inline"
-                ></i>
-                <i class="fa fa-circle fa-3x ml-3 text-muted d-inline"></i>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <img
-              class="img-fluid d-block"
-              src="https://static.pingendo.com/img-placeholder-tablet.svg"
-            />
           </div>
         </div>
       </div>
     </div>
-    <div class="py-5">
+    <div class="py-3">
       <div class="container">
         <div class="row">
-          <div class="px-5 col-md-12 text-center mx-auto" style="">
-            <h3 class="text-primary display-4 text-secondary">
-              <b>THE MASTER</b>
+          <div class="px-4 col-md-12 text-center mx-auto" style="">
+            <h3 class="display-4 text-secondary text-uppercase" style="">
+              Founder-Creator of Rise & The Wolf Project
             </h3>
-            <h2 class="my-3">I am so happy, my dear friend.</h2>
+            <h2 class="my-3 display-4">Kiel J Taylor</h2>
             <img
               class="img-fluid d-block d-inline-flex"
-              src="../assets/pictures/Picture1.png"
+              src="../assets/pictures/KielPic.png"
             />
           </div>
         </div>
@@ -160,7 +117,7 @@
         <div class="row">
           <div class="col-md-12 d-flex justify-content-center">
             <h3 class="text-primary display-4 text-secondary">
-              <b>YOU'LL LEARN HOW TO</b>
+              <b>WHAT YOU'LL LEARN</b>
             </h3>
           </div>
         </div>
@@ -171,20 +128,20 @@
                 <img
                   src="../assets/pictures/chart.svg"
                   class="bg-secondary rounded-circle mr-2 p-2 icon"
-                />Cras justo odio
+                />Strategic Marketing
               </li>
               <li class="list-group-item">
                 <img
                   src="../assets/pictures/choice.svg"
                   class="bg-secondary rounded-circle mr-2 p-2 icon"
                 />
-                Morbi leo risus
+                World View of Money
               </li>
               <li class="list-group-item">
                 <img
-                  src="../assets/pictures/diamond.svg"
+                  src="../assets/pictures/pie-chart.svg"
                   class="bg-secondary rounded-circle mr-2 p-2 icon"
-                />Porta ac consectetur ac
+                />Entrepreneur to CEO
               </li>
             </ul>
           </div>
@@ -194,19 +151,19 @@
                 <img
                   src="../assets/pictures/idea.svg"
                   class="bg-secondary rounded-circle mr-2 p-2 icon"
-                />Cras justo odio
+                />Grow Wealth
               </li>
               <li class="list-group-item">
                 <img
                   src="../assets/pictures/law.svg"
                   class="bg-secondary rounded-circle mr-2 p-2 icon"
-                />Morbi leo risus
+                />Protect Wealth
               </li>
               <li class="list-group-item">
                 <img
-                  src="../assets/pictures/pie-chart.svg"
+                  src="../assets/pictures/diamond.svg"
                   class="bg-secondary rounded-circle mr-2 p-2 icon"
-                />Porta ac consectetur ac
+                />Secure Wealth
               </li>
             </ul>
           </div>
@@ -214,54 +171,17 @@
         <div class="row">
           <div class="col-md-12 d-flex justify-content-center pt-3">
             <a class="btn btn-outline-secondary m-2 btn-lg py-3 px-5" href="#"
-              >YES, I WANT IN!</a
+              >SECURE YOUR SEAT</a
             >
           </div>
         </div>
       </div>
     </div>
-    <div class="py-5 text-center w-100 bg-secondary shadow-lg" style="">
-      <div class="container w-100">
-        <div class="row w-100">
-          <div class="mx-auto col-md-12">
-            <h1 class="display-3 text-light">EXPERT SPEAKERS</h1>
-            <div class="row">
-              <div class="col-md-4 d-flex justify-content-center">
-                <div class="col-lg-12 col-6 p-4 shadow-lg m-1">
-                  <img
-                    class="img-fluid d-block mb-3 mx-auto rounded-circle shadow-lg"
-                    src="https://static.pingendo.com/img-placeholder-1.svg"
-                    width="150"
-                    alt="Card image cap"
-                  />
-                  <h4 class="text-dark"><b>J. W. Goethe</b></h4>
-                  <p class="mb-0 text-secondary bg-dark">CEO and founder</p>
-                </div>
-              </div>
-              <div class="col-md-4 justify-content-center d-flex">
-                <div class="col-lg-12 col-6 p-4 m-1 mx-0 shadow-lg">
-                  <img
-                    class="img-fluid d-block mb-3 mx-auto rounded-circle shadow-lg"
-                    src="https://static.pingendo.com/img-placeholder-2.svg"
-                    width="150"
-                    alt="Card image cap"
-                  />
-                  <h4 class="text-dark"><b>G. W. John</b></h4>
-                  <p class="mb-0 text-secondary bg-dark">Co-founder</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <div class="py-5"><div class="row py-5 w-100"></div></div>
-
-    <div class="p-5 text-center container-fluid bg-secondary">
+    <div class="py-5 text-center container-fluid bg-secondary">
       <h1 class="display-3 text-light">EXPERT SPEAKERS</h1>
       <div class="row justify-content-center">
-        <div class="col-md-6 col-lg-8">
+        <div class="col-sm-12 col-md-10 col-lg-10">
           <div class="card-deck">
             <div class="card shadow-lg ">
               <div class="card-body p-3">
@@ -271,8 +191,15 @@
                   width="150"
                   alt="Card image cap"
                 />
-                <h3 class="card-title">Kevin Varble</h3>
-                <h5 class="card-subtitle">Coach</h5>
+                <h3 class="card-title" style="color:black;font-weight:bold">
+                  Kevin Varble
+                </h3>
+                <h4
+                  class="card-subtitle text-secondary bg-dark"
+                  style="font-weight:bold"
+                >
+                  Coach
+                </h4>
                 <p>
                   Kevin Varble is a Business Transformational coach and Public
                   Speaker. Since 2012, when Kevin started his career in the
@@ -295,8 +222,15 @@
                   width="150"
                   alt="Card image cap"
                 />
-                <h3 class="card-title">Kevin Varble</h3>
-                <h5 class="card-subtitle">Coach</h5>
+                <h3 class="card-title" style="color:black;font-weight:bold">
+                  Briton Hill
+                </h3>
+                <h4
+                  class="card-subtitle text-secondary bg-dark"
+                  style="font-weight:bold"
+                >
+                  VP/Partner Weber Global Management
+                </h4>
                 <p>
                   Kevin Varble is a Business Transformational coach and Public
                   Speaker. Since 2012, when Kevin started his career in the
@@ -323,163 +257,50 @@
       <div class="container py-0">
         <div class="row">
           <div class="col-md-8 mx-auto">
-            <h1 class="display-3 mb-4 text-secondary">
-              IS THE RISE EVENT FOR ME?
+            <h1 class="display-4 mb-4 text-secondary">
+              RISE MARKETING MOVEMENT MONEY
             </h1>
-            <p class="lead mb-5">
-              Heaven and earth seem to dwell in my soul and absorb its power,
-              like the form of a beloved mistress, then I often think with
-              longing, Oh, would I could describe these conceptions, could
-              impress upon paper all that is living.
-            </p>
-            <a class="btn btn-outline-secondary m-2 btn-lg py-3 px-5" href="#"
+            <a
+              class="btn btn-outline-secondary m-2 btn-lg py-3 px-3 wrap"
+              style="white-space: normal;"
+              href="#"
               >10X YOUR BUSINESS! REGISTER NOW!<br
             /></a>
           </div>
         </div>
       </div>
     </div>
-    <div class="py-3 bg-light">
+
+    <div class="py-5 text-center">
       <div class="container">
         <div class="row">
-          <div class="col-md-6">
-            <div class="card">
-              <div class="card-body p-4 bg-dark">
-                <p class="mb-3">
-                  <i
-                    >I throw myself down among the tall grass by the trickling
-                    stream; and, as I lie close to the earth, a thousand unknown
-                    plants are noticed by me: when I hear the buzz of the little
-                    world.</i
-                  >
-                </p>
-                <div class="row">
-                  <div class="col-lg-2 col-3" style="">
-                    <img
-                      class="img-fluid d-block rounded-circle"
-                      src="https://static.pingendo.com/img-placeholder-2.svg"
-                    />
-                  </div>
-                  <div
-                    class="col-lg-7 col-9 d-flex align-items-center"
-                    style=""
-                  >
-                    <p class="my-2 text-secondary">J. W. Goethe <i>CEO</i></p>
-                  </div>
+          <div class="col-lg-6 mx-auto">
+            <h1 class="">CAN'T MAKE IT ON THE {{ date }}!</h1>
+            <p class="mb-4 text-secondary">
+              Give Us Your Email So We Can Send You More RISE Events
+            </p>
+            <form class="form-inline d-flex justify-content-center">
+              <div class="input-group">
+                <input
+                  type="email"
+                  class="form-control form-control-lg"
+                  placeholder="Your email"
+                  id="form3"
+                />
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="button">
+                    Subscribe
+                  </button>
                 </div>
               </div>
-              <i><i> </i></i>
-            </div>
-            <i><i> </i></i>
-          </div>
-          <i><i> </i></i>
-          <div class="ml-auto col-md-6">
-            <i><i> </i></i>
-            <div class="card">
-              <i><i> </i></i>
-              <div class="card-body bg-dark p-4" style="">
-                <i
-                  ><i>
-                    <p class="mb-3">
-                      <i
-                        >Then, my friend, when darkness overspreads my eyes, and
-                        heaven and earth seem to dwell in my soul and absorb its
-                        power, like the form of a beloved mistress, then I often
-                        think with longing.</i
-                      >
-                    </p>
-                    <div class="row">
-                      <div class="col-lg-2 col-3">
-                        <img
-                          class="img-fluid d-block rounded-circle"
-                          src="https://static.pingendo.com/img-placeholder-3.svg"
-                        />
-                      </div>
-                      <div class="col-lg-10 col-9 d-flex align-items-center">
-                        <p class="my-2 text-secondary">
-                          G. W. John&nbsp; <i>Co-founder</i>
-                        </p>
-                      </div>
-                    </div>
-                  </i></i
-                >
-              </div>
-              <i><i> </i></i>
-            </div>
-            <i><i> </i></i>
-          </div>
-          <i><i> </i></i>
-        </div>
-        <i><i> </i></i>
-      </div>
-      <i><i> </i></i>
-    </div>
-    <div
-      class="py-5 text-center"
-      style="	background-image: url(D:/Documents/Company Work/Kiel - Rise Event/ocean-bg.jpg);	background-size: cover;	background-position: realative;	background-repeat: repeat;"
-    >
-      <div class="container">
-        <div class="row">
-          <div class="p-5 mx-auto col-md-8 col-10 bg-secondary">
-            <h3 class="display-3 text-dark">I feel the charm</h3>
-            <p class="mb-3 lead text-dark">Of existence in this spot</p>
-            <p class="mb-4 text-dark">
-              Which was created for the bliss of souls like mine. I am so happy,
-              my dear friend, so absorbed in the exquisite sense of mere
-              tranquil existence, that I neglect my talents.
+            </form>
+            <p style="color:var(--gray)">
+              We'll never share your email with anyone.
             </p>
-            <a
-              class="btn btn-lg btn-outline-dark justify-content-center d-flex"
-              href="#"
-              >SECURE YOUR SPOT BEFORE ITS TOO LATE<br
-            /></a>
           </div>
         </div>
       </div>
     </div>
-    <i
-      ><i>
-        <div class="py-5 text-center">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-6 mx-auto">
-                <h1 class="">CAN'T MAKE IT ON THE {{ date }}th!</h1>
-                <p class="mb-4 text-secondary">
-                  Enter your emial so we can send you more information for the
-                  next one. Trust me you won't want to miss this huge
-                  opportunity again!
-                </p>
-                <form class="form-inline d-flex justify-content-center">
-                  <div class="input-group">
-                    <input
-                      type="email"
-                      class="form-control form-control-lg"
-                      placeholder="Your email"
-                      id="form3"
-                    />
-                    <div class="input-group-append">
-                      <button class="btn btn-primary" type="button">
-                        Subscribe
-                      </button>
-                    </div>
-                  </div>
-                </form>
-                <p class="small">We'll never share your mail with anyone.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="py-4">
-          <div class="container">
-            <div class="row">
-              <div class="col-12">
-                <h2>Forms</h2>
-                <hr />
-              </div>
-            </div>
-          </div>
-        </div> </i
-    ></i>
   </div>
 </template>
 
@@ -490,8 +311,7 @@ export default {
   name: "Main",
   data() {
     return {
-      date: "9-10",
-      location: "conference center"
+      date: "2nd"
     };
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+/* eslint no-undef: 0 */
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
